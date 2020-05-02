fbbe53f0d71d88ba8c13c9d0d050a5ca78a9d705
Pedro Gonzalez <pgonzalez@post.com>
2020-05-01 21:05:07 -0500

    Agregar PeticionPeter perritos bebés Shina Inus

--- peticiones/index.js
+++ peticiones/index.js
@@ -1,9 +1,11 @@
 const PeticionMaui = require('./PeticionMaui');
 const PeticionEjemplo = require('./PeticionEjemplo');
 const getUserGitHub = require('./PeticionVictor');
+const PeticionPeter = require('PeticionPeter');
 
 module.exports = {
   PeticionMaui,
   PeticionEjemplo,
-  getUserGitHub
+  getUserGitHub,
+  PeticionPeter
 }
