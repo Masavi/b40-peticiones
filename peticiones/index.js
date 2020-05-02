2f084c0131b3080aeb40317d41278a4c493a57ce
alemanjosh7 <40409070+alemanjosh7@users.noreply.github.com>
2020-05-01 20:15:44 -0600

    Peticion de Random Users

--- peticiones/index.js
+++ peticiones/index.js
@@ -1,28 +1,19 @@
 const PeticionMaui = require('./PeticionMaui');
-<<<<<<< HEAD
 
 
 
 module.exports = {
-  PeticionMaui, PeticionJosue,
-=======
-const PeticionJosue = require('./PeticionJosue')
+PeticionMaui,
+const PeticionJosue = require('./PeticionJosue');
 const PeticionEjemplo = require('./PeticionEjemplo');
 const getUserGitHub = require('./PeticionVictor');
 const PeticionPeter = require('PeticionPeter');
 const DiasFestivos = require('./PeticionLuisDa');
-
+}
 module.exports = {
   PeticionMaui,
   PeticionJosue,
   PeticionEjemplo,
   getUserGitHub,
-<<<<<<< HEAD
-  DiasFestivos
->>>>>>> fd6907135e1e89c9662b9b649262d29f5f3c1d7a
+
 }
-=======
-  PeticionPeter,
-  DiasFestivos,
-}
->>>>>>> 686ba5ee94184a0c6b9947ee243a108c993194ae
