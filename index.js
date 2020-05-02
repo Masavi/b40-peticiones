34609a69f5bfa789a833c218e9167d4cdac06dfb
JESUA LUJAN <lujan.oficial@gmail.com>
2020-05-01 20:39:22 -0500

    PETICIONES INTERESANTES CON DEVF

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const axios = require('axios');
 const peticiones = require('./peticiones');
 
-const respuesta = peticiones.PeticionMaui(axios);
+
+const respuesta = peticiones.PeticionJesua(axios);
 respuesta.then(res => console.log(res.data));
+
+// const respuesta = peticiones.PeticionMaui(axios);
+// respuesta.then(res => console.log(res.data));
--- peticiones/index.js
+++ peticiones/index.js
@@ -1,5 +1,7 @@
 const PeticionMaui = require('./PeticionMaui');
+const PeticionJesua = require('./PeticionJesua');
 
 module.exports = {
   PeticionMaui,
+  PeticionJesua
 }
