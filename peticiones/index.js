78ef661e469cd578f2332892161252ce5bfc172c
rafaaro <rafa@arobyte.com>
2020-05-01 21:07:52 -0500

    add new peticion to index

--- peticiones/index.js
+++ peticiones/index.js
@@ -1,9 +1,11 @@
 const PeticionMaui = require('./PeticionMaui');
 const PeticionEjemplo = require('./PeticionEjemplo');
 const getUserGitHub = require('./PeticionVictor');
+const PeticionRafa = require('./PeticionRafa');
 
 module.exports = {
   PeticionMaui,
   PeticionEjemplo,
-  getUserGitHub
+  getUserGitHub,
+  PeticionRafa,
 }
